f426a34d898a18050a95427d57700a1eac984931
Alex Oh <oalex@knights.ucf.edu>
2022-02-05 19:57:50 -0500

    Part 1c Completed

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,57 +1,29 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React, { useState } from "react";
 
-const Header = (props) => {
-  return <h1>{props.course}</h1>;
-};
+const Display = ({ counter }) => <div>{counter}</div>;
 
-const Content = (props) => {
-  return (
-    <>
-      <Part name={props.parts[0].name} exercise={props.parts[0].exercises} />
-      <Part name={props.parts[1].name} exercise={props.parts[1].exercises} />
-      <Part name={props.parts[2].name} exercise={props.parts[2].exercises} />
-    </>
-  );
-};
-const Part = (props) => {
-  return (
-    <p>
-      {props.name} {props.exercise}
-    </p>
-  );
-};
+const Button = ({ onClick, text }) => <button onClick={onClick}>{text}</button>;
 
-const Total = (props) => {
-  return (
-    <p>
-      {props.parts[0].exercises +
-        props.parts[1].exercises +
-        props.parts[2].exercises}
-    </p>
-  );
-};
 const App = () => {
-  const course = "Half Stack application development";
-  const parts = [
-    {
-      name: "Fundamentals of React",
-      exercises: 10,
-    },
-    {
-      name: "Using props to pass data",
-      exercises: 7,
-    },
-    {
-      name: "State of a component",
-      exercises: 14,
-    },
-  ];
+  const [counter, setCounter] = useState(0);
+
+  const increaseByOne = () => {
+    setCounter(counter + 1);
+  };
+  const decreaseByOne = () => {
+    setCounter(counter - 1);
+  };
+
+  const setToZero = () => {
+    setCounter(0);
+  };
+
   return (
     <div>
-      <Header course={course} />
-      <Content parts={parts} />
-      <Total parts={parts} />
+      <Display counter={counter} />
+      <Button onClick={increaseByOne} text="plus" />
+      <Button onClick={setToZero} text="zero" />
+      <Button onClick={decreaseByOne} text="minus" />
     </div>
   );
 };
--- part1/src/index.js
+++ part1/src/index.js
@@ -1,4 +1,4 @@
-import ReactDOM from "react-dom";
+import reactDom from "react-dom";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+reactDom.render(<App />, document.getElementById("root"));
