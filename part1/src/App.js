ba0841f28d59a6d901c93d81c24bd6e45a72c11d
Alex Oh <oalex@knights.ucf.edu>
2022-02-07 18:28:17 -0500

    Exercise 1.6 Unicafe step1

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,26 +1,34 @@
 import React, { useState } from "react";
 
-const Button = (props) => (
-  <button onClick={props.handleClick}>{props.text}</button>
+const Button = (props) => {
+  return <button onClick={props.handleClick}>{props.text}</button>;
+};
+
+const Header = (props) => <h1>{props.text}</h1>;
+const Counter = (props) => (
+  <p>
+    {props.text} {props.number}
+  </p>
 );
-const Display = (props) => <div>{props.value}</div>;
-
 const App = () => {
-  const [value, setValue] = useState(10);
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
-  const setToValue = (newValue) => {
-    setValue(newValue);
-  };
-
-  // Do not define components inside another component
-  // const Display = (props) => <div>{props.value}</div>;
   return (
     <div>
-      <Display value={value} />
-      <Button handleClick={() => setToValue(1000)} text="thousand" />
-      <Button handleClick={() => setToValue(0)} text="reset" />
-      <Button handleClick={() => setToValue(value + 1)} text="increment" />
+      <Header text="give feedback" />
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <Header text="statistics" />
+
+      <Counter text="good " number={good} />
+      <Counter text="neutral " number={neutral} />
+      <Counter text="bad" number={bad} />
     </div>
   );
 };
+
 export default App;
