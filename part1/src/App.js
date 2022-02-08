9ee0368bcf6de102266da967b924b8da30924bae
Alex Oh <oalex@knights.ucf.edu>
2022-02-07 20:19:29 -0500

    Exercise 1.8 Unicafe step3

--- part1/src/App.js
+++ part1/src/App.js
@@ -5,11 +5,20 @@ const Button = (props) => {
 };
 
 const Header = (props) => <h1>{props.text}</h1>;
-const Counter = (props) => (
-  <p>
-    {props.text} {props.number}
-  </p>
-);
+// const Counter = (props) => (
+//   <p>
+//     {props.text} {props.number}
+//   </p>
+// );
+
+const Statistics = (props) => {
+  return (
+    <p>
+      {props.text} {props.number}
+    </p>
+  );
+};
+
 const App = () => {
   // save clicks of each button to its own state
   const [good, setGood] = useState(0);
@@ -24,19 +33,18 @@ const App = () => {
       <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
       <Button handleClick={() => setBad(bad + 1)} text="bad" />
       <Header text="statistics" />
-
-      <Counter text="good " number={good} />
-      <Counter text="neutral " number={neutral} />
-      <Counter text="bad " number={bad} />
-      <Counter text="average " number={good + neutral + bad} />
-      <Counter
+      <Statistics text="good " number={good} />
+      <Statistics text="neutral " number={neutral} />
+      <Statistics text="bad " number={bad} />
+      <Statistics text="average " number={good + neutral + bad} />
+      <Statistics
         text="average "
         number={(good + bad * -1) / (good + neutral + bad)}
       />
-      <Counter
+      <Statistics
         text="positive "
         number={(good / (good + bad + neutral)) * 100 + "%"}
-      ></Counter>
+      />
     </div>
   );
 };
