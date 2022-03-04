2c06589d11b51f4c41bc5b934b50dd541e35e6a7
Alex Oh <oalex@knights.ucf.edu>
2022-03-04 14:08:35 -0500

    Part1 course information review

--- part1/src/App.js
+++ part1/src/App.js
@@ -1,10 +1,71 @@
-import React, { useState } from "react";
+import React from "react";
 
+const Header = (props) => {
+  return <h1>{props.courseName}</h1>;
+};
+
+const Part = (props) => {
+  return (
+    <p>
+      {props.part} {props.exercises}
+    </p>
+  );
+};
+
+const Content = (props) => {
+  return (
+    <div>
+      <Part
+        part={props.courseContent[0].name}
+        exercise={props.courseContent[0].exercises}
+      />
+      <Part
+        part={props.courseContent[1].name}
+        exercise={props.courseContent[1].exercises}
+      />
+      <Part
+        part={props.courseContent[2].name}
+        exercise={props.courseContent[2].exercises}
+      />
+    </div>
+  );
+};
+
+const Total = (props) => {
+  return (
+    <p>
+      {props.courseContent[0].exercises +
+        props.courseContent[1].exercises +
+        props.courseContent[2].exercises}
+    </p>
+  );
+};
 const App = () => {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-  return <div></div>;
+  const course = {
+    name: "Half Stack application development",
+    parts: [
+      {
+        name: "Fundamentals of React",
+        exercises: 10,
+      },
+      {
+        name: "Using props to pass data",
+        exercises: 7,
+      },
+      {
+        name: "State of a component",
+        exercises: 14,
+      },
+    ],
+  };
+
+  return (
+    <div>
+      <Header courseName={course.name}></Header>
+      <Content courseContent={course.parts}></Content>
+      <Total courseContent={course.parts}></Total>
+    </div>
+  );
 };
 
 export default App;
